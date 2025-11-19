ddaaba379a3337b6280fc285f5868d1119dd1f2b
jangel234 <jaguilar57@ucol.mx>
2025-11-19 07:52:49 -0600

    artists views

--- app/api-client.ts
+++ app/api-client.ts
@@ -1,6 +1,6 @@
 import { ArtistResource } from "@/types/artists"
 
-const API_KEY = "a0b7538025b8a38c70ce8dd816798f6b"
+// const API_KEY = "a0b7538025b8a38c70ce8dd816798f6b"
 const URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=spain&api_key=a0b7538025b8a38c70ce8dd816798f6b&format=json`
 
 function getMusicData() {
